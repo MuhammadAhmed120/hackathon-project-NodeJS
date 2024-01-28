1ab3e1034be7427000e52130161cb502c1e23f9d
Muhammad Ahmed <muhammadahmed120192@gmail.com>
2024-01-28 16:32:58 +0500

    adding delete api

--- routes/userDetails.js
+++ routes/userDetails.js
@@ -21,34 +21,34 @@ router.get("/my-account", verifyToken, async (req, res) => {
         if (userData && userID) {
             return res.status(200).send({ status: 200, userData });
         } else {
-            return res.status(404).send({ status: 404, error: 'User not found.' })
+            return res.status(404).send({ status: 404, message: 'User not found.' })
         }
     } catch (error) {
-        return res.status(500).send({ status: 500, error: error.message })
+        return res.status(500).send({ status: 500, message: error.message })
     }
 })
 
-router.put("/", verifyToken, async (req, res) => {
-    try {
-        const { decodedToken, body } = req;
-        const userID = new mongoose.Types.ObjectId(decodedToken.customer_id);
+// router.put("/user-edit", verifyToken, async (req, res) => {
+//     try {
+//         const { decodedToken, body } = req;
+// const userID = new mongoose.Types.ObjectId(decodedToken.customer_id);
 
-        const updatedUser = await UserModel.findByIdAndUpdate(
-            userID,
-            body,
-            { new: true }
-        );
+//         const updatedUser = await UserModel.findByIdAndUpdate(
+//             userID,
+//             body,
+//             { new: true }
+//         );
 
-        if (updatedUser) {
-            return res.status(200).send({ status: 200, updatedUser, message: "Successfully updated." });
-        } else {
-            return res.status(404).send({ status: 404, error: 'User not found.' });
-        }
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send({ status: 500, error: error.message, message: "Internal Server Error" });
-    }
-});
+//         if (updatedUser) {
+//             return res.status(200).send({ status: 200, updatedUser, message: "Successfully updated." });
+//         } else {
+//             return res.status(404).send({ status: 404, error: 'User not found.' });
+//         }
+//     } catch (error) {
+//         console.error(error);
+//         return res.status(500).send({ status: 500, error: error.message, message: "Internal Server Error" });
+//     }
+// });
 
 
 
@@ -62,10 +62,29 @@ router.get("/all-users", async (req, res) => {
         if (allUserData) {
             return res.status(200).send({ status: 200, allUserData });
         } else {
-            return res.status(404).send({ status: 404, error: 'Users not found.' })
+            return res.status(404).send({ status: 404, message: 'Users not found.' })
         }
     } catch (error) {
-        return res.status(500).send({ status: 500, error: error.message })
+        return res.status(500).send({ status: 500, message: error.message })
+    }
+})
+
+router.post("/user-delete", verifyToken, async (req, res) => {
+    try {
+        console.log(req.body)
+        const { UID } = req.body
+
+        const userID = new mongoose.Types.ObjectId(UID);
+
+        const deleteUser = await UserModel.deleteOne({ _id: userID });
+
+        if (deleteUser) {
+            return res.status(200).send({ message: 'Deleted' })
+        }
+
+        return res.status(404).send({ message: 'User not deleted.' })
+    } catch (error) {
+        return res.status(400).send({ message: error })
     }
 })
 
@@ -82,10 +101,10 @@ router.get("/admin-account", verifyToken, async (req, res) => {
         if (panelData && panelID) {
             return res.status(200).send({ status: 200, panelData });
         } else {
-            return res.status(404).send({ status: 404, error: 'Admin not found.' })
+            return res.status(404).send({ status: 404, message: 'Admin not found.' })
         }
     } catch (error) {
-        return res.status(500).send({ status: 500, error: error.message })
+        return res.status(500).send({ status: 500, message: error.message })
     }
 })
 
